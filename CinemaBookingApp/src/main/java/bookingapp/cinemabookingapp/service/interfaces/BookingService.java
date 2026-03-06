5ee3513ef239c428648dfdce99f8087c29b005f0
Adewole Adedotun <erik.edwards.jon@gmail.com>
2026-03-06 06:05:46 -0800

    email notification now works

--- CinemaBookingApp/src/main/java/bookingapp/cinemabookingapp/service/interfaces/BookingService.java
+++ CinemaBookingApp/src/main/java/bookingapp/cinemabookingapp/service/interfaces/BookingService.java
@@ -1,10 +1,16 @@
 package bookingapp.cinemabookingapp.service.interfaces;
 
+import bookingapp.cinemabookingapp.data.models.Booking;
+import bookingapp.cinemabookingapp.data.models.Movies;
+import bookingapp.cinemabookingapp.data.models.Show;
 import bookingapp.cinemabookingapp.dtos.request.BookShowRequest;
 import bookingapp.cinemabookingapp.dtos.response.BookShowResponse;
+import bookingapp.cinemabookingapp.dtos.response.Receipt;
 
 public interface BookingService {
     BookShowResponse bookShow(BookShowRequest bookShowRequest);
 
     void unBook();
+
+    Receipt generateReceipt(String bookingId);
 }
