581be518818538a3d70a682817caf045cf9670ab
Adewole Adedotun <erik.edwards.jon@gmail.com>
2026-03-06 21:31:02 -0800

    webhook green

--- CinemaBookingApp/src/main/java/bookingapp/cinemabookingapp/dtos/request/BookShowRequest.java
+++ CinemaBookingApp/src/main/java/bookingapp/cinemabookingapp/dtos/request/BookShowRequest.java
@@ -1,10 +1,14 @@
 package bookingapp.cinemabookingapp.dtos.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class BookShowRequest {
+    @NotBlank(message = "can not be blank")
     private String showId;
+    @NotBlank(message = "can not be blank")
     private String email;
+    @NotBlank(message = "can not be blank")
     private String seatNumber;
 }
