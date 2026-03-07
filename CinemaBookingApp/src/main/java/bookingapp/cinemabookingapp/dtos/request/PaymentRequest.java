a5de592a23ff2196cf92ac35800d26077fe75096
Adewole Adedotun <erik.edwards.jon@gmail.com>
2026-03-06 21:30:56 -0800

    webhook green

--- CinemaBookingApp/src/main/java/bookingapp/cinemabookingapp/dtos/request/PaymentRequest.java
+++ CinemaBookingApp/src/main/java/bookingapp/cinemabookingapp/dtos/request/PaymentRequest.java
@@ -1,12 +1,18 @@
 package bookingapp.cinemabookingapp.dtos.request;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.math.BigDecimal;
 @Data
-public class PaymentRequest {
+public class PaymentRequest{
+    @NotBlank(message = "can not be blank")
     private String bookingId;
+    @Min(value = 50)
     private BigDecimal price;
+    @NotBlank(message = "can not be blank")
     private String reference;
 
 }
