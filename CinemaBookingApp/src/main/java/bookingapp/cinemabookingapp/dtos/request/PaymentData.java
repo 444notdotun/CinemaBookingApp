7e807b9471b41b95234533a5e33be15e6a671258
Adewole Adedotun <erik.edwards.jon@gmail.com>
2026-03-06 21:30:46 -0800

    webhook green

--- CinemaBookingApp/src/main/java/bookingapp/cinemabookingapp/dtos/request/PaymentData.java
+++ CinemaBookingApp/src/main/java/bookingapp/cinemabookingapp/dtos/request/PaymentData.java
@@ -1,9 +1,12 @@
 package bookingapp.cinemabookingapp.dtos.request;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class PaymentData {
+
     private String reference;
     private int amount;
     private String status;
