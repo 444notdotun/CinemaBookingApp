9adf6aedbbb3641e57224c61df5274cc1da9cb73
Adewole Adedotun <erik.edwards.jon@gmail.com>
2026-03-07 08:53:50 -0800

    controller green

--- CinemaBookingApp/src/main/java/bookingapp/cinemabookingapp/config/SecurityConfig.java
+++ CinemaBookingApp/src/main/java/bookingapp/cinemabookingapp/config/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig {
                 .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth->auth
                         .requestMatchers("/Admin/Login").permitAll()
-                        .requestMatchers("/paymentApi").permitAll()
+                        .requestMatchers("/user/*").permitAll()
                         .anyRequest().authenticated()
                 ) .exceptionHandling(ex -> ex
                         .authenticationEntryPoint((request, response, authException) ->
