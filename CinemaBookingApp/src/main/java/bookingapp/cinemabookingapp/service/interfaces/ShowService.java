636b9a3b885c0e8ba4d6d795af4d9444f79bf277
Adewole Adedotun <erik.edwards.jon@gmail.com>
2026-03-06 21:31:17 -0800

    webhook green

--- CinemaBookingApp/src/main/java/bookingapp/cinemabookingapp/service/interfaces/ShowService.java
+++ CinemaBookingApp/src/main/java/bookingapp/cinemabookingapp/service/interfaces/ShowService.java
@@ -1,9 +1,11 @@
 package bookingapp.cinemabookingapp.service.interfaces;
 
 import bookingapp.cinemabookingapp.dtos.response.ShowDescriptionResponse;
+import bookingapp.cinemabookingapp.dtos.response.ShowReponse;
 
 public interface ShowService {
 
     ShowDescriptionResponse ShowDescription(String id);
 
+    ShowReponse getShow(String showId);
 }
