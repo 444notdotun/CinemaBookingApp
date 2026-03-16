e911e8d9437fdea42f57c4f3394e4c3e960b2514
Adewole Adedotun <erik.edwards.jon@gmail.com>
2026-03-16 14:56:26 -0700

    refactored the superadmin creation

--- CinemaBookingApp/src/main/java/bookingapp/cinemabookingapp/CinemaBookingAppApplication.java
+++ CinemaBookingApp/src/main/java/bookingapp/cinemabookingapp/CinemaBookingAppApplication.java
@@ -4,6 +4,7 @@ import bookingapp.cinemabookingapp.data.models.Admin;
 import bookingapp.cinemabookingapp.data.models.Role;
 import bookingapp.cinemabookingapp.data.models.SuperAdmin;
 import bookingapp.cinemabookingapp.data.repository.AdminRepo;
+import lombok.extern.slf4j.Slf4j;
 import org.jspecify.annotations.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -14,6 +15,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 @EnableScheduling
+@Slf4j
 public class CinemaBookingAppApplication implements CommandLineRunner {
     @Autowired
     AdminRepo  adminRepo;
@@ -26,11 +28,13 @@ public class CinemaBookingAppApplication implements CommandLineRunner {
 
     @Override
     public void run(String @NonNull ... args){
-        Admin admin = new SuperAdmin("Superadmin1955","Superadminpassword", Role.SUPER_ADMIN);
-        admin.setPassword(passwordEncoder.encode(admin.getPassword()));
-        if(adminRepo.existsAdminByRole(admin.getRole())){
-            throw  new RuntimeException("Admin with id "+admin.getId()+" already exists");
+        if(!adminRepo.existsAdminByRole(Role.SUPER_ADMIN)){
+            Admin admin = new SuperAdmin("Superadmin1955","Superadminpassword", Role.SUPER_ADMIN);
+            admin.setPassword(passwordEncoder.encode(admin.getPassword()));
+            adminRepo.save(admin);
+            log.info("Admin has been saved successfully");
+        }else {
+            log.info("Admin exists already");
         }
-        adminRepo.save(admin);
     }
 }
