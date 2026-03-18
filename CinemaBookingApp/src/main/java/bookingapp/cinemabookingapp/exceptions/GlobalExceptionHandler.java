24360d1f422acf4f80d478575fbb0c5427fa5bbf
Adewole Adedotun <erik.edwards.jon@gmail.com>
2026-03-17 21:33:47 -0700

    GlobalExceptionHandler corrected

--- CinemaBookingApp/src/main/java/bookingapp/cinemabookingapp/exceptions/GlobalExceptionHandler.java
+++ CinemaBookingApp/src/main/java/bookingapp/cinemabookingapp/exceptions/GlobalExceptionHandler.java
@@ -9,16 +9,15 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class GlobalExceptionHandler {
     @ExceptionHandler(BookingNotFound.class)
     public ResponseEntity<?> bookingNotFound(Exception ex){
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
-
     @ExceptionHandler(AdminAlreadyExistException.class)
     public ResponseEntity<?> adminAlreadyExist(Exception ex){
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
     @ExceptionHandler(MovieNotFoundException.class)
     public ResponseEntity<?> movieNotFound(Exception ex){
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
     @ExceptionHandler(SeatDetailException.class)
     public ResponseEntity<?> seatDetail(Exception ex){
@@ -26,11 +25,11 @@ public class GlobalExceptionHandler {
     }
     @ExceptionHandler(TheaterNotFoundException.class)
     public ResponseEntity<?> theaterNotFound(Exception ex){
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
     @ExceptionHandler(ShowNotFoundException.class)
     public ResponseEntity<?> showNotFound(Exception ex){
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
     @ExceptionHandler(validatePaymentHeader.class)
     public ResponseEntity<?> validatePaymentHeader(Exception ex){
