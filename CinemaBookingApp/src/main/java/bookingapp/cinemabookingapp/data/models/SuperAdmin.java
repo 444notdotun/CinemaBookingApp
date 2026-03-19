427778d3ff947c259445339989306907f9141516
Adewole Adedotun <erik.edwards.jon@gmail.com>
2026-03-19 15:31:17 -0700

    correcting mongodb

--- CinemaBookingApp/src/main/java/bookingapp/cinemabookingapp/data/models/SuperAdmin.java
+++ CinemaBookingApp/src/main/java/bookingapp/cinemabookingapp/data/models/SuperAdmin.java
@@ -2,7 +2,7 @@ package bookingapp.cinemabookingapp.data.models;
 
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document
+@Document(collection = "admins")
 public class SuperAdmin extends Admin{
 
     public SuperAdmin(String id, String Password, Role role) {
